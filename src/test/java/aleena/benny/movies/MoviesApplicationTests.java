1b735eac843edf36f22695973459b48dec476657
Aleena Benny <aleenabennya@gmail.com>
2023-10-25 23:00:16 +0530

    backend changes

---
+++ src/test/java/aleena/benny/movies/MoviesApplicationTests.java
@@ -0,0 +1,13 @@
+package aleena.benny.movies;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MoviesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
